3745ccc77863bf1dd30903acec509c38ed401c39
bright_ngantcha20 <jbraxy2005@gmail.com>
2026-05-15 20:55:03 +0100

    fix: format alembic env.py files

--- services/notification/alembic/env.py
+++ services/notification/alembic/env.py
@@ -11,8 +11,10 @@ if config.config_file_name is not None:
 
 target_metadata = None
 
+
 def get_url():
     return os.getenv("DATABASE_URL", "sqlite:///./dev.db")
+
 
 def run_migrations_offline() -> None:
     url = get_url()
@@ -25,6 +27,7 @@ def run_migrations_offline() -> None:
     with context.begin_transaction():
         context.run_migrations()
 
+
 def run_migrations_online() -> None:
     configuration = config.get_section(config.config_ini_section, {})
     configuration["sqlalchemy.url"] = get_url()
@@ -34,11 +37,10 @@ def run_migrations_online() -> None:
         poolclass=pool.NullPool,
     )
     with connectable.connect() as connection:
-        context.configure(
-            connection=connection, target_metadata=target_metadata
-        )
+        context.configure(connection=connection, target_metadata=target_metadata)
         with context.begin_transaction():
             context.run_migrations()
+
 
 if context.is_offline_mode():
     run_migrations_offline()
